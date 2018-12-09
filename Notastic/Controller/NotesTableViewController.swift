e1f5c7c0ea7883a8c858a887ae5365fed54e1812
Bryce Poole <eggnof@mac.com>
2018-12-09 08:58:08 -0800

    Remove some un-needed comments.

--- Notastic/Controller/NotesTableViewController.swift
+++ Notastic/Controller/NotesTableViewController.swift
@@ -32,12 +32,10 @@ class NotesTableViewController: UITableViewController {
     /*****************************************************************************/
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return notes?.count ?? 1//Table should be equal to the number of items in noteArray
     }
     
